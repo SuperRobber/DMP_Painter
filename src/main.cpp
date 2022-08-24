ab21c1d410752182f079671bde4063c423edb9ba
Rob Bothof <rbothof@xs4all.nl>
2022-08-24 16:32:49 +0200

    status update

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,15 @@
-// #include "Display6963.h"
-#include <Arduino.h>
 #include "MotionControl.h"
 #include "PWMSounds.h"
-// #include "font.h"
+#include <Arduino.h>
 #include <SPI.h>
 
 /////////////////////////////
 
-elapsedMicros LCDRefreshTimer;
-elapsedMicros LOADRefreshTimer;
-elapsedMicros POWERRefreshTimer;
+// elapsedMicros POWERRefreshTimer;
 elapsedMillis disconnectTimer;
 elapsedMillis statusTimer;
+elapsedMillis requestTimer;
+// elapsedMillis drawTimeout;
 
 const int AudioPin = 33;
 // const int lcd_cmdPin = 23;
@@ -21,6 +19,62 @@ const int powersense_cs_Pin = 34;
 SPISettings spi_powersense_config(16000000, MSBFIRST, SPI_MODE0);
 int32_t powerData = 0;
 
+// DATA Layout of one message should be:
+//  - 10 x 0xFF byte header
+//  - 1 byte containing the size of the data to be received (N)
+//  - N number of values up to 8byte (int64) each, with a 0x00 byte in between
+//  - a checksum calculated on the original data.
+
+struct DrawInstruction {
+    int64_t index;
+    uint8_t type;
+    int8_t dirX;
+    int8_t dirY;
+    int64_t startX;
+    int64_t startY;
+    int64_t endX;
+    int64_t endY;
+    int64_t deltaX;
+    int64_t deltaY;
+    int64_t deltaXX;
+    int64_t deltaYY;
+    int64_t deltaXY;
+    int64_t error;
+    int64_t steps;
+};
+
+// union for converting int32 to bytearray
+union byte32 {
+    int32_t value;
+    byte bytes[4];
+};
+
+// union for converting int64 to bytearray
+union byte64 {
+    int64_t value;
+    byte bytes[8];
+};
+
+//======== CIRCULAR Buffer for drawinstructions ==========//
+volatile DrawInstruction iBuffer[64]; // use power of 2 size so I can use & in stead of modulo // ex tailIndex = (tailIndex + 1) & 63;
+volatile uint8_t iBufferWriteIndex = 0;
+volatile uint8_t iBufferReadIndex = 0;
+int64_t requestedInstruction = -1;
+int64_t recievedInstruction = -1;
+int requestCounter = 0;
+
+//======== Serial Binary Protocol ==========//
+int serialMessageSize = 0;
+int serialHeaderCount = 0;
+bool serialMessageStarted = false;
+int serialMessageByteCount = 0;
+int serialMessageChecksumByteCount = 0;
+int32_t serialMessageCalculatedChecksum = 0;
+byte32 serialMessageReceivedChecksum = {0};
+byte serialMessageData[250]; // reserve 250bytes for serialmessage data
+
+void getInstruction(int bytesToRead);
+
 void setup() {
     Serial.begin(115200);
 
@@ -28,13 +82,8 @@ void setup() {
     pinMode(powersense_cs_Pin, OUTPUT);
     digitalWriteFast(powersense_cs_Pin, HIGH);
 
-    // LCD and PowerSense are on SPI0
+    // PowerSense on SPI0
     SPI.begin();
-
-    // lcd_init(lcd_writePin, lcd_cmdPin);
-
-    // drawText(0, 0, "Startup: ZERO Position", msx_8x8_font, false);
-    // drawScreen();
 
     // playVictorySound(AudioPin);
     playBeep(AudioPin);
@@ -45,15 +94,48 @@ void setup() {
     StartEngines();
 
     playBeep(AudioPin);
-    // while (!Serial) {
-    // }
 }
 
 void loop() {
     if (Serial.dtr()) {
         // getInstructions();
+        int bytesToRead = Serial.available();
+        if (bytesToRead > 0) {
+            getInstruction(bytesToRead);
+        }
 
-        if (statusTimer > 50) {
+        // do we have room in the buffer ?
+        if (((iBufferWriteIndex + 1) & 63) != iBufferReadIndex) {
+            // have we received our latest request ?
+            if (recievedInstruction == requestedInstruction) {
+                // yes, lets request a new instruction to add into the buffer
+                requestedInstruction++;
+                Serial.print("@");
+                Serial.print(requestedInstruction);
+                Serial.println("@ requesting instruction ");
+                requestTimer = 0;
+            } else {
+                // no
+                if (requestTimer > 10000) {
+                    // its been 10seconds, lets re-request
+                    Serial.print("@");
+                    Serial.print(requestedInstruction);
+                    Serial.print("@ requesting instruction, ");
+                    Serial.print("retry: ");
+                    Serial.println(requestCounter);
+                    if (requestCounter > 20) {
+                        Serial.println("returning home.");
+                    } else {
+                        requestCounter++;
+                    }
+                    requestTimer = 0;
+                }
+            }
+        } else {
+            // buffer is full, wait for drawing to advance before requesting more
+        }
+
+        if (statusTimer > 50) { //20fps update
 
             SPI.beginTransaction(spi_powersense_config);
             digitalWriteFast(powersense_cs_Pin, LOW);
@@ -66,44 +148,44 @@ void loop() {
             statusTimer = 0;
             String status = "";
             status += "$status$";
-            status += String(M1_pos) ;
-            status +="$";
-            status += String(M2_pos) ;
-            status +="$";
-            status += String(M3_pos) ;
-            status +="$";
-            status += String(M4_pos) ;
-            status +="$";
-            status += String(M5_pos) ;
-            status +="$";
-            status += String(status_M1.Stallguard_Load) ;
-            status +="$";
-            status += String(status_M2.Stallguard_Load) ;
-            status +="$";
-            status += String(status_M3.Stallguard_Load) ;
-            status +="$";
-            status += String(0) ;
-            status +="$";
-            status += String(0) ;
-            status +="$";
-            status += String((powerData - 2047) * 12) ;
-            status +="$";
-            status += String(Limit_X1_start) ;
-            status +="$";
-            status += String(Limit_X1_end) ;
-            status +="$";
-            status += String(Limit_X2_start) ;
-            status +="$";
-            status += String(Limit_X2_end) ;
-            status +="$";
-            status += String(Limit_Y_start) ;
-            status +="$";
-            status += String(Limit_Y_end) ;
-            status +="$";
-            status += String(Limit_Z_start) ;
-            status +="$";
-            status += String(Limit_Z_end) ;
-            status +="$";
+            status += String(M1_pos);
+            status += "$";
+            status += String(M2_pos);
+            status += "$";
+            status += String(M3_pos);
+            status += "$";
+            status += String(M4_pos);
+            status += "$";
+            status += String(M5_pos);
+            status += "$";
+            status += String(status_M1.Stallguard_Load);
+            status += "$";
+            status += String(status_M2.Stallguard_Load);
+            status += "$";
+            status += String(status_M3.Stallguard_Load);
+            status += "$";
+            status += String(0);
+            status += "$";
+            status += String(0);
+            status += "$";
+            status += String((powerData - 2047) * 12);
+            status += "$";
+            status += String(Limit_X1_start);
+            status += "$";
+            status += String(Limit_X1_end);
+            status += "$";
+            status += String(Limit_X2_start);
+            status += "$";
+            status += String(Limit_X2_end);
+            status += "$";
+            status += String(Limit_Y_start);
+            status += "$";
+            status += String(Limit_Y_end);
+            status += "$";
+            status += String(Limit_Z_start);
+            status += "$";
+            status += String(Limit_Z_end);
+            status += "$";
             Serial.println(status);
         }
 
@@ -115,124 +197,133 @@ void loop() {
             // go home.
         }
     }
+}
 
-    // refresh display
-    // partially refresh screen about 3us/step
-    //< 8ms for a total screen redraw in 32x64 = 2048+12 steps
+void getInstruction(int bytesToRead) {
+    // read chars from serial port and convert to a byte array
+    char charbuffer[bytesToRead];
+    byte bytebuffer[bytesToRead];
+    Serial.readBytes(charbuffer, bytesToRead);
+    memcpy(bytebuffer, charbuffer, bytesToRead);
 
+    // iterate over all the bytes
+    for (int i = 0; i < bytesToRead; i++) {
+        if (!serialMessageStarted) {
+            if (serialHeaderCount == 10) {
+                // we have read exactly 10 startbytes (0xFF)
+                serialMessageStarted = true;
+                serialMessageSize = bytebuffer[i];
+                serialMessageByteCount = 0;
+                serialMessageChecksumByteCount = 0;
+                serialMessageReceivedChecksum.value = 0;
 
-    /*
-    drawScreenStep();
+            } else {
+                if (bytebuffer[i] == 0xFF) {
+                    serialHeaderCount++;
+                } else {
+                    // we did not receive a correct header, reset check
+                    serialHeaderCount = 0;
+                }
+            }
+        } else {
+            if (serialMessageByteCount < serialMessageSize) {
+                // add byte to our checksum
+                serialMessageCalculatedChecksum += bytebuffer[i];
+                serialMessageData[serialMessageByteCount] = bytebuffer[i];
+                serialMessageByteCount++;
+            } else {
+                // we should have received a complete message now, verify the checksum
+                // the next 4 bytes are the checksum
 
-    if (lcd_drawstep == 0) {
-        // copy from interrupt
-        uint32_t m1p = M1_pos;
-        uint32_t m2p = M2_pos;
-        uint32_t m3p = M3_pos;
-        uint32_t m4p = M4_pos;
-        uint32_t m5p = M5_pos;
-        // powerData=4000;
-        char m1_mm[10];
-        char m2_mm[10];
-        char m3_mm[10];
-        char m4_mm[10];
-        char m5_mm[10];
+                serialMessageReceivedChecksum.bytes[serialMessageChecksumByteCount] = bytebuffer[i];
+                serialMessageChecksumByteCount++;
 
-        char power[10];
+                if (serialMessageChecksumByteCount == 3) {
+                    requestTimer = 0;
+                    requestCounter = 0;
+                    if (serialMessageReceivedChecksum.value == serialMessageCalculatedChecksum) {
+                        // get Index
+                        byte64 receivedIndex = {};
+                        // memcpy(receivedIndex.bytes, bytebuffer + 11, 8); // nope we shoud use our serialMessageDATA!
+                        memcpy(receivedIndex.bytes, serialMessageData, 8);
 
-        dtostrf(((float)(m1p * 12) * PI) / (200.0f * 256.0f), 9, 2, m1_mm);
-        dtostrf(((float)(m2p * 12) * PI) / (200.0f * 256.0f), 9, 2, m2_mm);
-        dtostrf(((float)(m3p * 12) * PI) / (200.0f * 256.0f), 9, 2, m3_mm);
-        dtostrf(((float)(m4p * 12) * PI) / (200.0f * 256.0f), 9, 2, m4_mm);
-        dtostrf(((float)(m5p * 12) * PI) / (200.0f * 256.0f), 9, 2, m5_mm);
+                        Serial.print("Received #");
+                        Serial.print(receivedIndex.value);
+                        Serial.print(" :");
+                        Serial.print(serialMessageSize);
+                        Serial.print(" bytes, checksum ");
+                        Serial.print(serialMessageCalculatedChecksum);
+                        Serial.println(" ok!");
 
-        dtostrf((powerData - 2047) * 12, 5, 0, power);
+                        if (receivedIndex.value == requestedInstruction) {
+                            // All is good we have the requested instruction
+                            // Write it to the current index!
+                            byte64 b64 = {};
+                            // byte64 b32 = {};
 
-        // drawText(0,  0, "POSITION", msx_8x8_font,false);
-        drawText(0, 0, "M1", msx_8x8_font, false);
-        drawText(24, 0, m1_mm, msx_8x8_font, false);
-        drawFrame(94, 0, 200, 6);
-        drawSquare(96, 2, 96 + (1024 - status_M1.Stallguard_Load) / 10, 4);
-        drawFrame(212, 0, 218, 6);
-        drawFrame(220, 0, 226, 6);
-        // drawFrame(233,0,239,6);
-        if (Limit_X1_start)
-            drawSquare(214, 2, 216, 4);
-        if (Limit_X1_end)
-            drawSquare(222, 2, 224, 4);
+                            // grab all bytes and put them into drawinstruction struct
+                            // remember we interleaved bytes with zero bytes as binary protocol
 
-        drawText(0, 9, "M2", msx_8x8_font, false);
-        drawText(24, 9, m2_mm, msx_8x8_font, false);
-        drawFrame(94, 9, 200, 15);
-        drawSquare(96, 11, 96 + (1024 - status_M2.Stallguard_Load) / 10, 13);
-        drawFrame(212, 9, 218, 15);
-        drawFrame(220, 9, 226, 15);
-        // drawFrame(233,9,239,15);
-        if (Limit_X2_start)
-            drawSquare(214, 11, 216, 13);
-        if (Limit_X2_end)
-            drawSquare(222, 11, 224, 13);
+                            memcpy(b64.bytes, serialMessageData, 8);
+                            iBuffer[iBufferWriteIndex].index = b64.value;
+                            iBuffer[iBufferWriteIndex].type = serialMessageData[9];
+                            iBuffer[iBufferWriteIndex].dirX = serialMessageData[11];
+                            iBuffer[iBufferWriteIndex].dirY = serialMessageData[13];
+                            memcpy(b64.bytes, serialMessageData + 15, 8);
+                            iBuffer[iBufferWriteIndex].startX = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 24, 8);
+                            iBuffer[iBufferWriteIndex].startY = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 33, 8);
+                            iBuffer[iBufferWriteIndex].endX = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 42, 8);
+                            iBuffer[iBufferWriteIndex].endY = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 51, 8);
+                            iBuffer[iBufferWriteIndex].deltaX = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 60, 8);
+                            iBuffer[iBufferWriteIndex].deltaY = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 69, 8);
+                            iBuffer[iBufferWriteIndex].deltaXX = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 78, 8);
+                            iBuffer[iBufferWriteIndex].deltaYY = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 87, 8);
+                            iBuffer[iBufferWriteIndex].deltaXY = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 96, 8);
+                            iBuffer[iBufferWriteIndex].error = b64.value;
+                            memcpy(b64.bytes, serialMessageData + 105, 8);
+                            iBuffer[iBufferWriteIndex].steps = b64.value;
 
-        drawText(0, 18, "M3", msx_8x8_font, false);
-        drawText(24, 18, m3_mm, msx_8x8_font, false);
-        drawFrame(94, 18, 200, 24);
-        drawSquare(96, 20, 96 + (1024 - status_M3.Stallguard_Load) / 10, 22);
-        drawFrame(212, 18, 218, 24);
-        drawFrame(220, 18, 226, 24);
-        // drawFrame(233,18,239,24);
-        if (Limit_Y_start)
-            drawSquare(214, 20, 216, 22);
-        if (Limit_Y_end)
-            drawSquare(222, 20, 224, 22);
+                            iBufferWriteIndex = (iBufferWriteIndex + 1) & 63;
+                            recievedInstruction = receivedIndex.value;
 
-        drawText(0, 27, "M4", msx_8x8_font, false);
-        drawText(24, 27, m4_mm, msx_8x8_font, false);
-        drawFrame(94, 27, 200, 33);
-        drawSquare(96, 29, 96 + (1024 - 1024) / 10, 31);
-        drawFrame(212, 27, 218, 33);
-        drawFrame(220, 27, 226, 33);
-        // drawFrame(233,27,239,33);
-        if (Limit_Z_start)
-            drawSquare(214, 29, 216, 31);
-        if (Limit_Z_end)
-            drawSquare(222, 29, 224, 31);
+                            // we already checked this before sending a request
+                            if (((iBufferWriteIndex + 1) & 63) != iBufferReadIndex) {
+                            }
 
-        drawText(0, 36, "M4", msx_8x8_font, false);
-        drawText(24, 36, m5_mm, msx_8x8_font, false);
-        drawFrame(94, 36, 200, 42);
-        drawSquare(96, 38, 96 + (1024 - 1024) / 10, 40);
-        // drawFrame(212,36,218,42);
-        // drawFrame(233,36,239,42);
+                        } else {
+                            Serial.println("But index does not match!");
+                            Serial.println("requesting message again..");
+                            Serial.print("@");
+                            Serial.print(requestedInstruction);
+                            Serial.println("@ requesting instruction ");
+                        }
 
-        drawText(190, 57, power, msx_8x8_font, false);
-        drawText(226, 57, "mA", msx_8x8_font, false);
-        drawText(164, 57, "24v:", msx_8x8_font, false);
+                    } else {
+                        Serial.print("Received data, checksum ");
+                        Serial.print(serialMessageCalculatedChecksum);
+                        Serial.println(" is bad! message corrupt ?");
+                        Serial.println("requesting message again..");
+                        Serial.print("@");
+                        Serial.print(requestedInstruction);
+                        Serial.println("@ requesting instruction ");
+                    }
 
-        // drawText(0, 57, Limit_X1_start_press_count, msx_8x8_font, false);
-        // drawText(20, 57, Limit_X1_end_press_count, msx_8x8_font, false);
-        // drawText(40, 57, Limit_X2_start_press_count, msx_8x8_font, false);
-        // drawText(60, 57, Limit_X2_end_press_count, msx_8x8_font, false);
+                    // restart
+                    serialMessageStarted = false;
+                    serialHeaderCount = 0;
+                    serialMessageByteCount = 0;
+                    serialMessageCalculatedChecksum = 0;
+                }
+            }
+        }
     }
-
-    if (POWERRefreshTimer > 50000) {
-        POWERRefreshTimer = 0;
-        SPI.beginTransaction(spi_powersense_config);
-        digitalWriteFast(powersense_cs_Pin, LOW);
-        powerData = SPI.transfer16(0);
-        digitalWriteFast(powersense_cs_Pin, HIGH);
-        SPI.endTransaction();
-        // Serial.print("Power Usage @ 24v: ");
-        // Serial.print((powerData-2047) * 12);
-        // Serial.println(" mA");
-
-        updateStepperStatus();
-        // Serial.print("M1 Load: ");
-        // Serial.println(1024-status_M1.Stallguard_Load);
-        // Serial.print("M2 Load: ");
-        // Serial.println(1024-status_M2.Stallguard_Load);
-        // Serial.print("M3 Load: ");
-        // Serial.println(1024-status_M3.Stallguard_Load);
-    }
-
-    */
 }
